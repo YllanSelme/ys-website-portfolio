f1c6ff2d6530b279907b9d988cf6464df16ff45d
YllanSelme <yllanselme@gmail.com>
2023-12-29 10:03:58 +0100

    insta react
    react & tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-blue-500 text-white p-4 text-center">
+        Yllan Selme
+      </div>
+    </div>
+  );
+}
+
+export default App;
